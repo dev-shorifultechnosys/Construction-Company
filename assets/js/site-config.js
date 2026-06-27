e794e3c1376427de10d424291b7913165baa9df1
Sheikh Shoriful Islam <sheikhshorifulislambadsha@gmail.com>
2026-06-27 17:32:12 +0600

    Add files via upload

---
+++ assets/js/site-config.js
@@ -0,0 +1,13 @@
+/**
+ * BULADEV + ASA editable site integrations.
+ * Add approved profile/map URLs only. Blank values remain hidden automatically.
+ */
+window.BULADEV_SITE_CONFIG = {
+  social: {
+    facebook: "",
+    instagram: "",
+    linkedin: "",
+    youtube: ""
+  },
+  googleMapsEmbedUrl: ""
+};
